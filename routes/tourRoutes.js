7a45b0a0dcef099e2026abda6859b87f9d89cc16
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-02-24 22:41:07 +0300

    new route added

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,9 +3,15 @@ const tourController = require("./../controllers/tourController");
 
 const router = express.Router();
 
-// router.param("id", tourController.checkID);
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
+router.route('/tour-stats')
+  .get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year')
+  .get(tourController.getMonthlyPlan);
 
 // CRUD routes for tour
 router
