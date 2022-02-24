c5a611e0cf6d582b29ae4c686abf8296d1b44cf7
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-02-24 05:42:32 +0300

    script for importing data from file to db

---
+++ dev-data/data/import_data_for_dev.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Tour = require("../../models/tourModel");
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+   process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL) // local Database connection
+  // .connect(DB) // remote Database connection
+  .then((con) => {
+    // console.log(con.connections);
+    console.log("DB connection successful");
+  });
+
+// READ JSON FILE
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data loading successful!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data deletion successful!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+// console.log(process.argv);
