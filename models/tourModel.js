098c4879caa1cca2d8a785257b13db874bd684e9
absaGH <78029486+absaGH@users.noreply.github.com>
2022-02-24 22:46:41 +0300

    Update tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim: true
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'A tour must have a duration']
@@ -56,8 +58,22 @@ const tourSchema = new mongoose.Schema({
     select: false
   },
   startDates: [Date]
+},
+{
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
