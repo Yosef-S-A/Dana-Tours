b8f458abcb68d5158c0b18083aa6514a7b222d29
Yosef-S-A <yosefsamuel22@gmail.com>
2022-02-25 06:06:48 +0300

    update to package.json

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+//app.use("/api/v1/users", userRouter);
 
 app.all('*', (req, res, next) => {
   res.status(404).json({
