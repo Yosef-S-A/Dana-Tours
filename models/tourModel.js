159b3b4e92abc0aa85168dd04e7d1c4c71a53b5c
Yosef-S-A <yosefsamuel22@gmail.com>
2022-02-26 02:44:23 +0300

    fixed error

--- models/tourModel.js
+++ models/tourModel.js
@@ -41,7 +41,16 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price']
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        // this only points to current doc on NEW document creation
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below regular price'
+    }
+  },
   summary: {
     type: String,
     trim: true,
