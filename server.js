b04d6ed2d7695dc084f9903ad5c2434fdd82f738
absaGH <78029486+absaGH@users.noreply.github.com>
2022-02-25 22:14:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
@@ -9,17 +15,23 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-console.log(mongoose.version);
-
 mongoose
   .connect(process.env.DATABASE_LOCAL) // local Database connection
   // .connect(DB) // remote Database connection
   .then((con) => {
-    // console.log(con.connections);
-    console.log("DB connection successful");
-  });
+  })
+  .then(() => console.log('DB connection successful!'));
+
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err=> {
+  console.log('UNHANDLER REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
