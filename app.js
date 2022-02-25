857bfba74238922855c5c5beab4df80602b2de10
absaGH <78029486+absaGH@users.noreply.github.com>
2022-02-25 05:44:56 +0300

    handle unknown uri

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
 const app = express();
 
 // MIDDLEWARES
@@ -18,4 +21,14 @@ app.use((req, res, next) => {
   next();
 });
 
+// ROUTES
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message:  `Can't find ${req.originalUrl} on this server!`
+  })
+})
 module.exports = app;
