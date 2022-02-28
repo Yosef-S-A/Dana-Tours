fdf2f618b9673aa87d76e9479052ac266267ff07
Yosef-S-A <yosefsamuel22@gmail.com>
2022-02-28 21:51:31 +0300

    hide unnecessary data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,7 +31,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 // get a tour by id
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate('guides');
+  const tour = await Tour.findById(req.params.id).populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
   // Tour.findOne({ _id: req.params.id })
 
   if (!tour) {
