2c9437216e6346008d92f79a4340e74ac59ac92a
Yosef-S-A <yosefsamuel22@gmail.com>
2022-02-28 19:26:23 +0300

    added location

--- models/tourModel.js
+++ models/tourModel.js
@@ -75,8 +75,38 @@ const tourSchema = new mongoose.Schema({
   secretTour: {
       type: Boolean,
       default: false
-    }
-},
+    },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
 {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -93,9 +123,9 @@ tourSchema.pre('save', function(next) {
 });
 
 tourSchema.pre('save', async function(next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
+ const guidesPromises = this.guides.map(async id => await User.findById(id));
+ this.guides = await Promise.all(guidesPromises);
+ next();
 });
 
 // QUERY MIDDLEWARE
