2c0b776cf51ab61f9acdf757275793389bd98802
Yosef-S-A <yosefsamuel22@gmail.com>
2022-03-01 06:53:28 +0300

    userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,18 +6,18 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+// Protect all routes
+router.use(authController.protect);
 
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
