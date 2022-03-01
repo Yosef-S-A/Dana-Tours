ef6c9cceadfd4fbec7264083a1a1c4f64fd53f75
Yosef-S-A <yosefsamuel22@gmail.com>
2022-03-01 08:58:17 +0300

    utils

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,35 +1,37 @@
 class APIFeatures {
-  constructor(query, queryStr) {
+  constructor(query, queryString) {
     this.query = query;
-    this.queryStr = queryStr;
+    this.queryString = queryString;
   }
 
   filter() {
-    const queryObj = { ...this.queryStr };
+    const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    this.query = this.query.find(JSON.parse(queryString));
+    this.query = this.query.find(JSON.parse(queryStr));
 
     return this;
   }
 
   sort() {
-    if (this.queryStr.sort) {
-      const sortBy = this.queryStr.sort.split(',').join(' ');
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
     }
 
     return this;
   }
 
   limitFields() {
-    // usage: uri?fields=field_1,field_2
-    if (this.queryStr.fields) {
-      const fields = this.queryStr.fields.split(',').join(' ');
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
       this.query = this.query.select(fields);
     } else {
       this.query = this.query.select('-__v');
@@ -39,8 +41,8 @@ class APIFeatures {
   }
 
   paginate() {
-    const page = this.queryStr.page * 1 || 1;
-    const limit = this.queryStr.limit * 1 || 100;
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
@@ -48,5 +50,4 @@ class APIFeatures {
     return this;
   }
 }
-
 module.exports = APIFeatures;
--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async options => {
-  // 1) Create a transporter
+  // Create a transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
@@ -11,16 +11,16 @@ const sendEmail = async options => {
     }
   });
 
-  // 2) Define the email options
+  // Define the email options
   const mailOptions = {
-    from: 'Yosef Samuel <yosefsam33@gmail.com',
+    from: 'Jonas Schmedtmann <hello@jonas.io>',
     to: options.email,
     subject: options.subject,
     text: options.message
     // html:
   };
 
-  // 3) Actually send the email
+  // Actually send the email
   await transporter.sendMail(mailOptions);
 };
 
