4262b091c3382e6c9d4de647bcad9dc3bcd34fe6
Yosef-S-A <yosefsamuel22@gmail.com>
2022-03-03 17:21:18 +0300

    models

--- models/tourModel.js
+++ models/tourModel.js
@@ -137,8 +137,6 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-// QUERY MIDDLEWARE
-// tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
 
