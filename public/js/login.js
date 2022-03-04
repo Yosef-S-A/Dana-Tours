7f4a374fffb27873c8e35fdb07817fd7a9217870
Yosef-S-A <yosefsamuel22@gmail.com>
2022-03-04 16:28:52 +0300

    login/logout implemented and refactored mapbox.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,41 +1,22 @@
 /* eslint-disable */
-// import axios from 'axios';
-// import { showAlert } from './alerts';
 
-// export const login = async (email, password) => {
-//   try {
-//     const res = await axios({
-//       method: 'POST',
-//       url: 'http://127.0.0.1:3000/api/v1/users/login',
-//       data: {
-//         email,
-//         password
-//       }
-//     });
+// DOM Element
+const loginForm = document.querySelector('.form');
+const logoutBtn = document.querySelector('.nav__el--logout');
 
-//     if (res.data.status === 'success') {
-//       showAlert('success', 'Logged in successfully!');
-//       window.setTimeout(() => {
-//         location.assign('/');
-//       }, 1500);
-//     }
-//   } catch (err) {
-//     showAlert('error', err.response.data.message);
-//   }
-// };
 
-// export const logout = async () => {
-//   try {
-//     const res = await axios({
-//       method: 'GET',
-//       url: 'http://127.0.0.1:3000/api/v1/users/logout'
-//     });
-//     if ((res.data.status = 'success')) location.reload(true);
-//   } catch (err) {
-//     console.log(err.response);
-//     showAlert('error', 'Error logging out! Try again.');
-//   }
-// };
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
+
+// type is 'success' or 'error'
+const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
+};
 
 const login = async (email, password) => {
   console.log(email, password);
@@ -48,15 +29,41 @@ const login = async (email, password) => {
         password
       }
     });
+    if(result.data.status === 'success'){
+      showAlert('success', 'Logged in successfully');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
     console.log(result)
   } catch(err) {
-    console.log(err.response.data);
+    showAlert('error', err.response.data.message);
   }
 };
 
-document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+
+if (loginForm) {
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
+
+const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout'
+    });
+    if ((res.data.status = 'success')) location.reload(true);
+  } catch (err) {
+    console.log(err.response);
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
+
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', logout);
+}
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,25 +1,53 @@
 /* eslint-disable */
-import axios from 'axios';
-import { showAlert } from './alerts';
+
+const userDataForm = document.querySelector('.form-user-data');
 
 // type is either 'password' or 'data'
-export const updateSettings = async (data, type) => {
-  try {
-    const url =
-      type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+// const updateSettings = async (data, type) => {
+//   try {
+//     const url =
+//       type === 'password'
+//         ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
+//         : 'http://127.0.0.1:3000/api/v1/users/updateMe';
 
+//     const res = await axios({
+//       method: 'PATCH',
+//       url,
+//       data
+//     });
+
+//     if (res.data.status === 'success') {
+//       showAlert('success', `${type.toUpperCase()} updated successfully!`);
+//     }
+//   } catch (err) {
+//     showAlert('error', err.response.data.message);
+//   }
+// };
+
+const updateData = async (name, email) => {
+  try {
     const res = await axios({
       method: 'PATCH',
-      url,
-      data
+      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
+      data: {
+        name,
+        email
+      }
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', `${type.toUpperCase()} updated successfully!`);
+      showAlert ('success', 'Data updated successfully!')
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
   }
-};
+}
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+    updateData(name, email);
+  });
+}
