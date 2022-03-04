ff57a9d40accb69db24366a387694047e7f73815
Yosef-S-A <yosefsamuel22@gmail.com>
2022-03-04 17:43:37 +0300

    update password feature

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // type is either 'password' or 'data'
 // const updateSettings = async (data, type) => {
@@ -24,19 +25,22 @@ const userDataForm = document.querySelector('.form-user-data');
 //   }
 // };
 
-const updateData = async (name, email) => {
-  try {
+// type -> either 'password' or 'data'
+const updateSettings = async (data, type) => {
+  try { 
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email
-      }
+      url,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert ('success', 'Data updated successfully!')
+      showAlert ('success', `${type.toUpperCase()} updated successfully!`)
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
@@ -48,6 +52,26 @@ if (userDataForm) {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    updateData(name, email);
+    updateSettings({name, email}, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async e => {
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+
   });
 }
