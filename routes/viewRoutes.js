98ad543c5c72d50a4b25765980121fff5e9fc0f2
Yosef-S-A <yosefsamuel22@gmail.com>
2022-03-09 14:58:24 +0300

    booking

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -25,9 +25,11 @@ router.use((req, res, next) => {
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
-router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/signup', authController.isLoggedIn, viewsController.getSingupForm);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
   '/submit-user-data',
